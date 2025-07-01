41742808e80376ab1caa9895bc33eddb85a6f8bb
vedantshetti <vedantshetti123456@gmail.com>
2025-07-01 10:32:51 +0530

    impemented the features of function

--- index.php
+++ index.php
@@ -1,32 +1,9 @@
-<!-- multidimensional array -->
-
 <?php
 
 
-$students = array(
-    array(
-        "age" => 20,
-        "grade" => "A",
-        "subjects" => "Math"
-    ),
-     array(
-        "age" => 22,
-        "grade" => "B",
-        "subjects" => "History",
-    ),
-     array(
-        "age" => 21,
-        "grade" => "C",
-        "subjects" =>  "Chemistry"
-    )
-);
-
-
-
-foreach ($students as $name=>$value){
-    foreach($value as $v => $v1){
-        echo $v . "=>" . $v1 . "<br>";
-    }
+function myFun(){
+    echo "weloce to my function";
 }
 
+myFun();
 ?>
